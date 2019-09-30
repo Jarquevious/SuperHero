2c5744571a5c39ba932e1a249705f76bd949c8b0
Jarquevious <jarquevious@gmail.com>
2019-09-29 17:32:47 -0700

    Replace File

--- superhero.py
+++ superhero.py
@@ -1,24 +1,38 @@
 import random 
+
 class Ability:
-    def __init__(self, force, attack_strength):
-        '''Create Instance Variables:
-          name:String
-          max_damage: Integer
-       '''
-        self.name = force  
-        self.attack_strength = 5
-       #TODO: Instantiate the variables listed in the docstring with then
-       # values passed in
-    
+    def __init__(self, name, mxdmg):
+        '''
+        Create Instance Variables:
+        name:String
+        max_damage: Integer
+        '''
+        self.name = name 
+        self.mxdmg = mxdmg
+
+        
     def attack(self):
-      ''' Return a value between 0 and the value set by self.max_damage.'''
-      # TODO: Use random.randint(a, b) to select a random attack value.
-      # Return an attack value between 0 and the full attack.
-      # Hint: The constructor initializes the maximum attack value.
-    #random.randint(0, 5) 
-        return random.randint(0, 5)
+        return random.randint(0, self.mxdmg)
+
+class Armor:
+    def __init__(self, name, maxx_block):
+        self.name = name
+        self.maxx_block = maxx_block
+
+    def block(self):
+        block = random.randint(0, self.max_block)
+        return block
+class Hero:
+    def __init__(self, name, starting_health=100):
+        self.abilities: []
+        self.armors: []
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.current_health: starting_health 
+
 
 if __name__ == "__main__":
-            ability = Ability("Debugging Ability", 20)
-            print(ability.name)
-            print(ability.attack())
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
