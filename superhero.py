6e8088cbde9bd7e691b18f605d9fd770288de2f2
Jarquevious <jarquevious@gmail.com>
2019-09-29 21:49:22 -0700

    Added new functions

--- superhero.py
+++ superhero.py
@@ -24,15 +24,39 @@ class Armor:
         return block
 class Hero:
     def __init__(self, name, starting_health=100):
-        self.abilities: []
-        self.armors: []
+        self.abilities = []
+        self.armors = []
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
         self.current_health: starting_health 
+   
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+        
+
+    def attack(self):
+        for ability in self.abilities:
+            return ability.attack()
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def defend(self, damage_amt):
+        for block in self.armors
+        return block
+
+    def 
+
+    
+
 
 
 if __name__ == "__main__":
-    my_hero = Hero("Black Panther", 200)
-    print(my_hero.name)
-    print(my_hero.current_health)
+    ability = Ability("Punch", 90)
+    another_ability = Ability("Love", 100)
+    hero = Hero("Black Panther", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
+    
