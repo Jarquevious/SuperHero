281fdb957152aab98f466fce35b379267345b387
Jarquevious <jarquevious@gmail.com>
2019-10-05 21:50:57 -0700

    test complete

--- superheroes.py
+++ superheroes.py
@@ -82,37 +82,105 @@ class Hero:
                     print(f'YEEEEEHAAAAA!! {opponent.name} is the WINNER!!!!')
 
 class Team:
-    def init(self, team_name):
-        self.name = team_name
+    def __init__(self, name):
+        ''' Initialize your team with its team name
+        '''
+        # TODO: Implement this constructor by assigning the name and heroes, which should be an empty list
+        self.name = name
         self.heroes = []
 
-    def add_hero(self, hero):
-       self.heroes.append(hero)
-   
     def remove_hero(self, name):
-       for hero in self.heroes:
-            if hero.name == name:
+        '''Remove hero from heroes list.
+        If Hero isn't found return 0.
+        '''
+        for hero in self.heroes:
+            if name == hero.name:
                 self.heroes.remove(hero)
-                return
-            return 0
+                return 1
+        return 0
 
     def view_all_heroes(self):
+        '''Prints out all heroes to the console.'''
+        # TODO: Loop over the list of heroes and print their names to the terminal.
         for hero in self.heroes:
             print(hero.name)
 
-    
-    
-    
+    def add_hero(self, hero):
+        '''Add Hero object to self.heroes.'''
+        # TODO: Add the Hero object that is passed in to the list of heroes in
+        # self.heroes
+        self.heroes.append(hero)
 
+    def attack(self, opponents):
+        ''' Sets up each time to check if they are alive and if they are it will set them up to fight.'''
 
+        alive_heroes = []
+        alive_opponents = []
+
+        for i in self.heroes:
+                if i.status == "alive":
+                    alive_heroes.append(self.heroes.index(i))
         
+        for x in opponents.heroes:
+                if x.status == "alive":
+                    alive_opponents.append(opponents.heroes.index(x))
 
+        while len(alive_heroes) > 0 and len(alive_opponents) > 0:
+            random_hero_1 = self.heroes[random.choice(alive_heroes)]
+            random_hero_2 = opponents.heroes[random.choice(alive_opponents)]
 
+            random_hero_1.fight(random_hero_2)
 
+            for death1 in self.heroes:
+                if death1.status == "dead":
+                    alive_heroes.pop(self.heroes.index(death1))
+            
+            for death2 in opponents.heroes:
+                if death2.status == "dead":
+                    alive_opponents.pop(opponents.heroes.index(death2))
+        
+        if len(alive_heroes) > 0:
+            return self.name
+        elif len(alive_opponents) > 0:
+            return opponents.name
+        elif len(alive_heroes) == len(alive_opponents):
+            return "Draw!"
 
-    
+    def update_kills(self, kills):
+        """Updates a heroes kill count for kills made in team battles """
+        for hero in self.heroes:
+            hero.add_kill(kills)
 
+    def alive_heroes(self):
+        '''Makes a list of alive heroes from team '''
+        alive_heroes = []
+        for hero in self.heroes:
+            if hero.is_alive():
+                alive_heroes.append(hero)
+            return alive_heroes
 
+    def revive_heroes(self, health=100):
+        ''' Reset all heroes health to starting_health'''
+        # TODO: This method should reset all heroes health to their
+        # original starting value.
+        for hero in self.heroes:
+            hero.current_health = 100
+            hero.status = "Alive"
+
+    def find_hero(self, name):
+        """Find index of specific hero on a team """
+        hero_index = -1
+        for x, hero in enumerate(self.heroes):
+            if hero.name == name:
+                hero_index = x
+        return hero_index
+
+    def stats(self):
+        '''Print team statistics'''
+
+        for hero in self.heroes:
+            print("------------------------------------")
+            print("Hero: {} | Kills: {} : | Deaths: {}".format(hero.name, hero.kills, hero.deaths))
 
 if __name__ == "__main__":
     hero1 = Hero("Black Panther")
